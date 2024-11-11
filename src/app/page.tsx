1f39a43957326b3838ab197df3054255c719185f
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-11 19:43:11 -0400

    [feat]: initial pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import MovieList from "@/components/MovieList/MovieList";
+import { SearchHero } from "@/components/SearchHero";
+import { MoviesProvider } from "@/context/MoviesContext";
+
 export default function App() {
-	return <div>Hello</div>;
+	return (
+		<main className="font-sans">
+			<MoviesProvider>
+				<SearchHero />
+				<MovieList />
+			</MoviesProvider>
+		</main>
+	);
 }

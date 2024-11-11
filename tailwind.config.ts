baa68879da8226e4a67122fb8ebcdd17012cccc6
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-11 18:10:53 -0400

    [chore]: global.css and layout cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"black": "#0a0a0a",
+				"dark-gray": "#18181C",
+			},
+			fontFamily: {
+				sans: ["var(--font-roboto-flex)", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;

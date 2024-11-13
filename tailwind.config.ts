518d4ef1a634064f76891f4a5b5cdcba69c23e58
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-13 17:37:00 -0400

    [feat]: added new styles for buttons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,33 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				"black": "#0a0a0a",
+				black: "#0a0a0a",
 				"dark-gray": "#18181C",
+				primary: {
+					DEFAULT: "#dc2626", // red-600
+					hover: "#b91c1c", // red-700
+					dark: "#991b1b", // red-800
+				},
 			},
 			fontFamily: {
 				sans: ["var(--font-roboto-flex)", "sans-serif"],
+			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: "1rem",
+					sm: "2rem",
+					lg: "4rem",
+					xl: "5rem",
+				},
+			},
+			keyframes: {
+				shimmer: {
+					"100%": { backgroundPosition: "-100% 0" },
+				},
+			},
+			animation: {
+				shimmer: "shimmer 2s infinite",
 			},
 		},
 	},

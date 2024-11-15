7b52894a9047f20be24afd4df71f16823249c200
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-15 15:31:04 -0400

    [fix]: changed correct url for redirection

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
 		return [
 			{
 				source: "/services/:path*",
-				destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+				destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
 			},
 		];
 	},

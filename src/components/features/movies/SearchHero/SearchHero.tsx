010f852c9067cd8044a4fad844e48392ca5bbfbd
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-18 12:04:45 -0400

    [fix] removed linear gradient in searchbox

--- src/components/features/movies/SearchHero/SearchHero.tsx
+++ src/components/features/movies/SearchHero/SearchHero.tsx
@@ -15,8 +15,8 @@ export default function SearchHero() {
 				<HeroContent />
 
 				<div className="relative max-w-3xl mx-auto">
-					<div className="absolute -inset-1 bg-gradient-to-r from-red-900 to-red-950 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
-					<div className="relative bg-dark-gray rounded-lg">
+					<div className="absolute -inset-1 bg-gradient-to-r  rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
+					<div className="relative bg-transparent rounded-lg">
 						<Suspense fallback={<Loader />}>
 							<SearchBox />
 						</Suspense>

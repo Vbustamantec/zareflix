8dc959f599fa35c89c5d77d252938f3b9b61aa11
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-18 12:05:06 -0400

    [fix]: completely removed gradient

--- src/components/features/movies/SearchHero/SearchHero.tsx
+++ src/components/features/movies/SearchHero/SearchHero.tsx
@@ -15,7 +15,6 @@ export default function SearchHero() {
 				<HeroContent />
 
 				<div className="relative max-w-3xl mx-auto">
-					<div className="absolute -inset-1 bg-gradient-to-r  rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
 					<div className="relative bg-transparent rounded-lg">
 						<Suspense fallback={<Loader />}>
 							<SearchBox />

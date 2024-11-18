c0cdee55b9cc3fbab05ab0d752fd6f40c4395b23
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-18 18:11:39 -0400

    [fix]: changed redirection for user when is not logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,17 @@ import { getSession } from "@auth0/nextjs-auth0/edge";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
+	if (req.nextUrl.pathname.startsWith("/favorites")) {
+		const res = NextResponse.next();
+		const session = await getSession(req, res);
+
+		if (!session) {
+			return NextResponse.redirect(`${process.env.NEXT_PUBLIC_FRONTEND_URL}`);
+		}
+
+		return res;
+	}
+
 	if (
 		req.nextUrl.pathname.startsWith("/proxy") ||
 		req.nextUrl.pathname.startsWith("/movie")
@@ -20,5 +31,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/proxy/:path*", "/movie/:path*"],
+	matcher: ["/proxy/:path*", "/movie/:path*", "/favorites/:path*"],
 };

8b9530a431e768608f7311d60e90c3b34f561ca1
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-18 12:50:56 -0400

    [chore]: rollback middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,20 @@ import { getSession } from "@auth0/nextjs-auth0/edge";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-	const res = NextResponse.next();
-	const session = await getSession(req, res);
+	if (req.nextUrl.pathname.startsWith("/proxy")) {
+		const res = NextResponse.next();
+		const session = await getSession(req, res);
 
-	if (!session) {
-		const loginUrl = new URL("/api/auth/login", req.url);
-		loginUrl.searchParams.set("redirectTo", req.nextUrl.pathname);
-		return NextResponse.redirect(loginUrl);
+		if (session?.accessToken) {
+			res.headers.set("Authorization", `Bearer ${session.accessToken}`);
+		}
+
+		return res;
 	}
 
-	if (req.nextUrl.pathname.startsWith("/proxy") && session?.accessToken) {
-		res.headers.set("Authorization", `Bearer ${session.accessToken}`);
-	}
-
-	return res;
+	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/proxy/:path*", "/:path*"],
+	matcher: ["/proxy/:path*"],
 };
